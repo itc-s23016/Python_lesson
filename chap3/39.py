02810c3399511705523770643f5c4e92ab154861
Namito Tsuhako <s23016@std.it-collega.ac.jp>
2023-05-24 07:09:42 +0000

    p39-40

---
+++ chap3/39.py
@@ -0,0 +1,5 @@
+home = "America"
+if home == "japan":
+    print("hello,America")
+else:
+    print("hello,world")
---
+++ chap3/htbu6g3.py
@@ -0,0 +1,5 @@
+home = "America"
+if home == "America":
+    print("hello,America")
+else:
+    print("hello,world")
